76497756e3dbbc8248b9eba8827014c1441fb60c
tonousa <tonousa@hotmail.com>
2015-08-06 19:51:28 -0400

    444

---
+++ ControllerExtensibility/Models/Result.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ControllerExtensibility.Models
+{
+    public class Result
+    {
+        public string ControllerName { get; set; }
+        public string ActionName { get; set; }
+    }
+}
