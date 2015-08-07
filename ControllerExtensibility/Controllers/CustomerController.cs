aa8b5c0b1a949e7e26ec0e240e4d346602596335
tonousa <xgood12>
2015-08-07 17:44:03 -0400

    461

---
+++ ControllerExtensibility/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using ControllerExtensibility.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerExtensibility.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+
+        public ActionResult Index()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Customer",
+                ActionName = "Index"
+            });
+        }
+
+        
+        [ActionName("Enumerate")]
+        public ActionResult List()
+        {
+            return View("Result", new Result
+            {
+                ControllerName = "Customer",
+                ActionName = "List"
+            });
+        }
+
+        [NonAction]
+        public ActionResult MyAction()
+        {
+            return View();
+        }
+    }
+}
